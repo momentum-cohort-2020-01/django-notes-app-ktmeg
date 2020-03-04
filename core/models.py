5d19f080b5d4592857e3ff3ae53da86b744a25d3
Kat <kmneil2@gmail.com>
2020-03-04 16:40:00 -0500

    debug added

--- core/models.py
+++ core/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+# from django.utils.text import slugify
 
 class Note(models.Model):
     title = models.CharField(max_length=50) 
     body = models.TextField()
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now=True)
+    # tag = models.ForeignKey('Tag', on_delete=models.DO_NOTHING, null=True, blank=True)
 
     def __str__(self):
         return f"Note title: {self.title} body: {self.body}"
-    
+
+# class Tag(models.Model):
+#     name = models.CharField(max_length=30)
+#     slug = slugify(name)
